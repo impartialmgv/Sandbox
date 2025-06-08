23147e46be91633e61f739d3f5233b962cb60ef8
mGv73 <nordstrom.max.2010@gmail.com>
2025-06-08 19:21:25 +0200

    TicTacToe Added

--- Project/Modules/RandomColor/scripts.js
+++ Project/Modules/RandomColor/scripts.js
@@ -1,40 +1,19 @@
-const dice = document.getElementById("dice");
-const diceNumber = document.getElementById("diceNumber");
-var scrambleActive = false;
+const button = document.getElementById("btn");
+const colorShow = document.getElementById("colorShow");
 
-dice.addEventListener("mouseover", function () {
-  if (scrambleActive == false)
-    dice.style.animation = "shake 0.1s linear 0s infinite alternate none";
-});
-dice.addEventListener("mouseout", function () {
-  dice.style.animation = "none";
-});
+button.addEventListener("mousedown", generate);
 
-dice.addEventListener("mousedown", roll);
-
-function roll() {
-  if (scrambleActive == false) {
-    scrambleActive = true;
-    dice.style.color = "white";
-    dice.style.animation = "none";
-    randomNum(10, 100);
-  }
+function generate() {
+  colorString =
+    "rgb(" +
+    Math.floor(Math.random() * (255 - 1) + 1) +
+    ", " +
+    Math.floor(Math.random() * (255 - 1) + 1) +
+    ", " +
+    Math.floor(Math.random() * (255 - 1) + 1) +
+    ")";
+  document.body.style.backgroundColor = colorString;
+  colorShow.innerHTML = colorString;
 }
 
-function randomNum(amount, interval) {
-  for (let index = 0; index < amount; index++) {
-    setTimeout(() => {
-      diceNumber.innerHTML = Math.floor(Math.random() * (6 - 1) + 1);
-    }, index * interval);
-  }
-  setTimeout(() => {
-    dice.style.color = "rgb(190, 0, 190)";
-    diceNumber.innerHTML = Math.floor(Math.random() * (6 - 1) + 1);
-  }, amount * interval);
-
-  setTimeout(() => {
-    diceNumber.innerHTML = "?";
-    dice.style.color = "white";
-    scrambleActive = false;
-  }, amount * interval + 3000);
-}
+diceNumber.innerHTML = Math.floor(Math.random() * (6 - 1) + 1);
